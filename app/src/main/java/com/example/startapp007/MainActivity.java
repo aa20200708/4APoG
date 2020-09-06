36392dd10d6f991130470ac2fed10f2998232b39
aa20200708 <aa20200708@gmail.com>
2020-09-06 22:43:55 +0300

    Change MainAcnivity and add ApiInterface

--- app/src/main/java/com/example/startapp007/MainActivity.java
+++ app/src/main/java/com/example/startapp007/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.startapp007;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<List<ClassFromJsonSchema>> call = apiService.getClassFromJsonSchemas();
+        call.enqueue(new Callback<List<ClassFromJsonSchema>>() {
+            @Override
+            public void onResponse(Call<List<ClassFromJsonSchema>> call, Response<List<ClassFromJsonSchema>> response) {
+            }
+
+            @Override
+            public void onFailure(Call<List<ClassFromJsonSchema>> call, Throwable t) {
+                Log.d("TAG","Response = "+t.toString());
+            }
+        });
     }
 }
